44629727506e945230ff610df46dc58aa41d0bed
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2024-10-13 08:44:26 -0700

    select htm elements by DOM

---
+++ script.js
@@ -0,0 +1,20 @@
+//GETTING INPUTS WITH DOM
+
+const firstName = document.getElementById("first_name");
+const lastName = document.getElementById("last_name");
+const age = document.getElementById("age");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm_password");
+const form = document.getElementById("form");
+const checkBox = document.getElementById("checkbox");
+
+//GETTING INPUT ERRORS
+
+const firstName_error = document.getElementById("first_name_error");
+const lastName_error = document.getElementById("last_name_error");
+const age_error = document.getElementById("age_error");
+const email_error = document.getElementById("email_error");
+const password_error = document.getElementById("password_error");
+const confirmPassword_error = document.getElementById("confirm_password_error");
+const checkbox_error = document.getElementById("checkbox_error");
